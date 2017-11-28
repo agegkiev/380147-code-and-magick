72f668b916779054f0afc1194d75fbccb5fd4d57
agegkiev <gegkiev@bk.ru>
2017-11-28 03:49:18 +0300

    добавить цвет, имена и значения

--- js/stat.js
+++ js/stat.js
@@ -11,6 +11,31 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
 
-  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillRect(120, 100, 40, 150);
+  var currentIndex = 0;
+  var max = times[currentIndex];
+  var maxIndex = -1;
+
+  for (var i = currentIndex + 1; i < times.length; i++) {
+    if (max < times[i]) {
+      max = (Math.round(times[i] * 10) / 10);
+      maxIndex = i;
+    }
+  }
+
+  var histogramHeight = 150;
+  var step = histogramHeight / (max - 0);
+  var myColor = 'rgba(255, 0, 0, 1)';
+  var histogramWidth = 40;
+  var jump = 50;
+  var baseY = 240;
+  var baseX = 120;
+  var lineHeight = 15;
+
+  for (var i = 0; i < times.length; i++){
+    ctx.fillRect(baseX, baseY, histogramWidth, -(times[i] * step));
+//    ctx.fillText(names[i], baseX += 90 * i + histogramWidth, baseY + lineHeight + jump * i);
+  }
+
+console.log(names)
+
 }
