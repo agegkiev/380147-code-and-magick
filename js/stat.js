662371c78ff6521531f336a41a51e45284cc6850
agegkiev <gegkiev@bk.ru>
2017-11-28 17:48:10 +0300

    fix

--- js/stat.js
+++ js/stat.js
@@ -1,27 +1,27 @@
 window.renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
 
-  ctx.fillStyle = "white";
+  ctx.fillStyle = 'white';
   ctx.fillRect(100, 10, 420, 270);
 
-  ctx.fillStyle = "black"
+  ctx.fillStyle = 'black';
   ctx.font = '16px PT Mono';
 
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
 
   var currentIndex = 0;
-  var max = times[currentIndex];
+  var maximum = times[currentIndex];
 
   for (var i = currentIndex + 1; i < times.length; i++) {
-    if (max < times[i]) {
-      max = (Math.round(times[i] * 10) / 10);
+    if (maximum < times[i]) {
+      maximum = (Math.round(times[i] * 10) / 10);
     }
   }
 
   var histogramHeight = 150;
-  var step = histogramHeight / (max - 0);
+  var step = histogramHeight / (maximum - 0);
   var myColor = 'rgba(255, 0, 0, 1)';
   var histogramWidth = 40;
   var jump = 50;
@@ -34,18 +34,18 @@ window.renderStatistics = function (ctx, names, times) {
       return myColor;
     }
     return anyColor;
-  }
+  };
 
   function getRandomInRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  for (var i = 0; i < times.length; i++) {
+  for (var j = 0; j < times.length; j++) {
     getRandomInRange(0.5, 1);
     anyColor = 'rgba(0, 0, 255, ' + getRandomInRange(0.1, 1) + ')';
     ctx.fillStyle = getColor();
-    ctx.fillRect(baseX + (jump + histogramWidth) * i, baseY, histogramWidth, -(times[i] * step));
-    ctx.fillText(names[i], baseX + (jump + histogramWidth) * i, baseY + lineHeight);
-    ctx.fillText((Math.round(times[i] * 10) / 10), baseX + (jump + histogramWidth) * i, baseY -(times[i] * step) - lineHeight );
+    ctx.fillRect(baseX + (jump + histogramWidth) * j, baseY, histogramWidth, -(times[j] * step));
+    ctx.fillText(names[j], baseX + (jump + histogramWidth) * j, baseY + lineHeight);
+    ctx.fillText((Math.round(times[j] * 10) / 10), baseX + (jump + histogramWidth) * j, baseY - (times[j] * step) - lineHeight);
   }
-}
+};
